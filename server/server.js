db1eaad711c3737cfff5e46e258a13d6681fc5b3
Samuel Whittenberger <sbw98978@gmail.com>
2022-12-01 14:55:23 -0500

    working submit endpoint

--- server/server.js
+++ server/server.js
@@ -10,10 +10,14 @@ server.use(express.urlencoded({ extended: true }));
 server.get("/api/supervisors", (req, res) => {
   axios.get("https://o3m5qixdng.execute-api.us-east-1.amazonaws.com/api/managers")
     .then((response) => {
-      console.log(response.data);
       res.send(response.data);
     })
     .catch(err => res.send(err));
+});
+
+server.put("/api/submit", (req, res) => {
+  console.log(req.body.formData);
+  res.status(200).send("success");
 });
 
 server.listen(PORT, () => console.log(`listening on port ${PORT}`));
--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import axios from 'axios';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
-
+  const [formData, setFormData] = useState(null);
   useEffect(() => {
     axios.get('/api/supervisors').then((response) => console.log(response.data));
   }, []);
+
+  function submitForm() {
+    setFormData({test: 'test'});
+    axios.put('/api/submit', {formData: formData});
+  }
 
   return (
     <div className="main-content">
       <div className="form-container">
         <div>Title</div>
         <input type='text'></input>
-        <button>Submit</button>
+        <button onClick={submitForm}>Submit</button>
       </div>
     </div>
   );
